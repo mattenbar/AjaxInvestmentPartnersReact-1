adf615b3de285fd77829f903358be5ad47252a58
mattenbar <matthw.enbar@gmail.com>
2020-12-07 17:03:47 -0500

    line thickness

--- src/components/blueMountains.js
+++ src/components/blueMountains.js
@@ -7,7 +7,11 @@ class BlueMountains extends React.Component{
     return(
       <div className="capabilities-img-container">
         <img width="100%" src={blueMountainsImg} alt="BlueMountainImg"/>
-        <div className="centered">WE SEED GREAT IDEAS FROM PROVEN LEADERS. <br/><br/> WE ACCELERATE YOUR GROWTH TRAJECTORY. <br/><br/> WE HELP DRIVE YOUR FUTURE.</div>
+        <div className="centered">WE&nbsp;&nbsp;SEED&nbsp;&nbsp;GREAT&nbsp;&nbsp;IDEAS&nbsp;&nbsp;&nbsp;FROM&nbsp;&nbsp;PROVEN&nbsp;&nbsp;LEADERS. 
+        <br/><br/> 
+        WE&nbsp;&nbsp;ACCELERATE&nbsp;&nbsp;YOUR&nbsp;&nbsp;GROWTH&nbsp;&nbsp;TRAJECTORY. 
+        <br/><br/> 
+        WE&nbsp;&nbsp;HELP&nbsp;&nbsp;DRIVE&nbsp;&nbsp;YOUR&nbsp;&nbsp;FUTURE.</div>
     </div>
     )
   }
